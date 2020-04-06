8c0fdf9ab137667d4f0c6e3472550b6c42b84c9d
Shashwat Pandey <cool17shashwat@gmail.com>
2020-04-06 18:42:46 +0530

    Create index.js

---
+++ src/client/index.js
@@ -0,0 +1,21 @@
+import { checkForName } from './js/nameChecker'
+import { handleSubmit } from './js/formHandler'
+
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
+import './styles/resets.scss'
+
+
+const apiEndpoint = 'https://api.aylien.com/api/v1';
+
+/*
+console.log(checkForName);
+alert("I EXIST")
+console.log("CHANGE!!");*/
+
+export {
+    checkForName,
+    handleSubmit
+};
